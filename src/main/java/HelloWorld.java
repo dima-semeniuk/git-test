c27bfc19c9cc6076a35c1d8353a049ca4ba0f700
Dima Semeniuk <semeniuk.dima@ukr.net>
2021-12-04 16:51:26 +0200

    Hello mates was added

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         String hi = "Hello, world!";
         System.out.println(hi);
+        String mate = "Hello mates!";
+        System.out.println(mate);
     }
 }
